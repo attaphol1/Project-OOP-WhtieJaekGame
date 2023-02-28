a2afb56dc0ea6718d4f653fd261aae08b24baf77
Sunwatcha303 <watcharakorn301146@gmail.com>
2023-03-01 01:25:08 +0700

    countdown

--- src/WaitingForConnection/TestMethod.java
+++ src/WaitingForConnection/TestMethod.java
@@ -1,73 +1,42 @@
 package src.WaitingForConnection;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.plaf.FontUIResource;
 
 public class TestMethod {
+    static int i;
+    static Timer t;
     public static void main(String[] args) {
         JFrame f = new JFrame("test");
         JLabel lb1 = new JLabel();
-        lb1.setText("page 1");
+        lb1.setText("return in 5");
         lb1.setBounds(200, 200, 100, 100);
-        new Test2(f);
+        i = 5;
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                lb1.setText("return in "+Integer.toString(seti()));
+                f.repaint();
+            }    
+        },1000,1000);
         
         f.add(lb1);
         f.setLayout(null);
         f.setSize(800, 600);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
-}
-
-class Test2{
-    JFrame fT2;
-    JButton btn;
-    JLabel lb;
-    int cnt;
-
-    Test2(JFrame f1){
-        btn = new JButton("HI");
-        lb = new JLabel();
-        fT2 = f1;
-        cnt= 0;
-        initBtn();
-        initLabel();
-        initFrame();
-    }
-
-    void initLabel(){
-        lb.setText(String.format("%d", cnt));
-    }
-
-    void initFrame(){
-        btn.setBounds(100, 100, 50, 50);
-        lb.setBounds(200, 100, 100, 100);
-        fT2.add(btn);
-        fT2.add(lb);
-        fT2.repaint();
-    }
-
-    void initBtn(){
-        btn.addActionListener(new ActionListener(){
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cnt++;
-                // TODO Auto-generated method stub
-                // fT2.removeAll();
-                fT2.remove(lb);
-                initLabel();
-                fT2.repaint();
-            } 
-        });
+    private static final int seti() {
+        //if interval is 1, cancel
+        if (i == 1) t.cancel();
+        return --i;
     }
 }
